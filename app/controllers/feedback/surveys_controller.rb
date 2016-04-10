0aea5aaf0d7c0d6fa25ecb7b11330ecc26199140
Ariel Caplan <ariel.caplan@vitals.com>
2016-04-10 23:34:33 +0300

    Add code to store submitted survey responses

--- app/controllers/feedback/surveys_controller.rb
+++ app/controllers/feedback/surveys_controller.rb
@@ -2,7 +2,18 @@ require_dependency "feedback/application_controller"
 
 module Feedback
   class SurveysController < ApplicationController
+    def create
+      Feedback::SurveyResponse.create!(survey_response_params)
+      redirect_to thanks_path
+    end
+
     def thanks
+    end
+
+    private
+
+    def survey_response_params
+      params.require(:survey_response).permit(:approval)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Feedback::Engine.routes.draw do
   get 'thanks', to: 'surveys#thanks'
+  post 'survey_responses', to: 'surveys#create'
 end
